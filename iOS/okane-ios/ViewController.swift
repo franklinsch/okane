965d635afc87b1fe2ab86cb5c62ccb3b9d07f2ab
Franklin Schrans <fs2014@ic.ac.uk>
2015-12-12 13:58:27 +0000

    Initial import with Models for user profiles

---
+++ iOS/okane-ios/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  okane-ios
+//
+//  Created by Franklin Schrans on 11/12/2015.
+//  Copyright © 2015 Franklin Schrans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
