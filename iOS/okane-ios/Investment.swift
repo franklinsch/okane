666540590eeacfebbdaa530feebc03f1893bd0be
Franklin Schrans <fs2014@ic.ac.uk>
2015-12-12 16:20:41 +0000

    Created classes for UIViews

---
+++ iOS/okane-ios/Investment.swift
@@ -0,0 +1,28 @@
+//
+//  Investment.swift
+//  okane-ios
+//
+//  Created by Saurav Mitra on 12/12/2015.
+//  Copyright (c) 2015 Franklin Schrans. All rights reserved.
+//
+
+class Investment {
+    var investid: INVESTID
+    var requestid: REQUESTID
+    var userid: USERID
+    var amountInvested: Int
+    var date: Date
+    
+    init(requestid: REQUESTID, userid: USERID, amountInvested: Int, date: Date) {
+        self.requestid = requestid
+        self.userid = userid
+        self.amountInvested = amountInvested
+        self.date = date
+        
+        self.investid = generateID()
+    }
+    
+    func generateID() -> INVESTID {
+        return 0;
+    }
+}
