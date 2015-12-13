2fcc717895b34471808340e1aff8b8dc52c49412
Heiki Riisikamp <hr1514@ic.ac.uk>
2015-12-13 04:00:24 +0000

    Added nodejs backend files + website assets + braintree api code for ios

---
+++ backend/app.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static('assets'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+var braintree = require("braintree");
+
+var gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "ffdkybmfrh2gkhvs",
+  publicKey: "jsx9dmc6kjnc3ng3",
+  privateKey: "6fe0af112496b4af016889b474cd9866"
+});
+
+
+app.get("/client_token", function (req, res) {
+  gateway.clientToken.generate({}, function (err, response) {
+    res.send(response.clientToken);
+    console.log("Sent the token");
+  });
+});
+
+app.post("/checkout", function (req, res) {
+  var nonce = req.body.payment_method_nonce;
+  // console.log(req.body.amount);
+  gateway.transaction.sale({
+    amount: req.body.amount,
+    paymentMethodNonce: nonce,
+  }, function (err, result) {
+    if (!err) {
+      console.log("successful");
+      res.send('index.html');  
+    } else {
+      console.log("error");
+    }
+  });
+});
+
+
+app.get('/', function (req, res) {
+  res.send('index.html');
+});
+
+var users = ["56241a14de4bf40b17112dbd", "56241a14de4bf40b17112dca", 
+"56241a14de4bf40b17112dcb", "56241a14de4bf40b17112dbc", 
+"56241a14de4bf40b17112dbb", "56241a14de4bf40b17112dba"];
+
+function makeTransaction(to, from, sum, resp) {
+  var postData = {
+    "medium": "balance",
+    "payee_id": from,
+    "amount": sum,
+    "transaction_date": "2015-12-12",
+    "status": "pending",
+    "description": "string"
+  };
+  console.log(postData);
+  request({
+    url: 'http://api.reimaginebanking.com/accounts/' + to + '/transfers?key=c1b8b3094ebc402ba35af8c1b8e3aa1a', //URL to hit
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    json: postData
+  }, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(response);
+      //resp.send(response);
+    }
+  });
+}
+
+app.get('/create', function (req, res) {
+  
+  users.forEach(function(senderId) {
+    users.forEach(function(receiverId) {
+      makeTransaction(senderId, receiverId, Math.floor((Math.random() * 3000)), res);
+    });
+  });
+  res.send("done");
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
